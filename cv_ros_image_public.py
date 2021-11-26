3d9042784503a006f771aa9ff276985466287d64
Danya <example.com>
2021-11-26 18:43:56 +0300

    asdfas

---
+++ cv_ros_image_public.py
@@ -0,0 +1,21 @@
+import numpy as py
+import cv2 as cv
+import rospy
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
+
+rospy.init_node('png')
+image_pub = rospy.Publisher('~debug', Image)
+
+bridge = CvBridge()
+
+def image_callback(data):
+    #img = cv.imread('shapes.jpg')
+    img = cv.imread('cirk.png')
+    #imgGrey = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    image_pub.publish(bridge.cv2_to_imgmsg(img, 'bgr8'))
+    
+    print(".") 
+
+image_sub = rospy.Subscriber('/main_camera/image_raw', Image, image_callback)
+rospy.spin()
